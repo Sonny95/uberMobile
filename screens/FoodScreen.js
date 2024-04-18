cbe6a716af42e31a2b9d6e2f20ee614acc352bfb
Sonny95 <thswlgml9598@gmail.com>
2024-04-19 00:38:08 +1000

    uber eats

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -1,30 +1,71 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import MapView from "react-native-maps";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
-import RideOptionsCard from "../components/RideOptionsCard";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const FoodScreen = () => {
   const data = [
-    { id: "1", icon: "beer", title: "Beer" },
-    { id: "2", icon: "pizza", title: "Pizza" },
-    { id: "3", icon: "fast-food-outline", title: "Burger" },
+    { id: "1", icon: "beer", title: "Beer", destination: "network Street, Sydney,AU" },
+    { id: "2", icon: "pizza", title: "Pizza", destination: "george Street, Sydney,AU" },
+    {
+      id: "3",
+      icon: "fast-food-outline",
+      title: "Burger",
+      destination: "bridge Street, Sydney,AU",
+    },
+    { id: "4", icon: "beer", title: "Drink", destination: "network Street, Sydney,AU" },
+    { id: "5", icon: "pizza", title: "Italian", destination: "george Street, Sydney,AU" },
+    {
+      id: "6",
+      icon: "fast-food-outline",
+      title: "Fast food",
+      destination: "bridge Street, Sydney,AU",
+    },
   ];
 
   const Stack = createNativeStackNavigator();
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <SafeAreaView>
-      {data.map((item) => (
-        <Icon type="ionicon" key={item.id} name={item.icon} /> // Icon 컴포넌트에 name 속성으로 아이콘 이름을 전달합니다.
-      ))}
+      {/* customer location */}
+      <View style={tw`h-1/3`}>
+        <View style={tw`p-5 flex flex-row items-center `}>
+          <Icon type="ionicon" name="navigate-outline" style={tw`mr-3`}></Icon>
+          <Text>{origin.description}</Text>
+        </View>
+
+        <View style={tw`bg-gray-200 w-full py-5`}>
+          <ScrollView
+            horizontal
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+          >
+            <View style={tw`flex flex-row items-center`}>
+              {data.map((item) => (
+                <View>
+                  <Icon type="ionicon" key={item.id} name={item.icon} size={60} style={tw`m-2`} />
+                  <Text key={origin.id} style={tw`flex text-center text-lg font-bold`}>
+                    {item.title}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+      <View style={tw`h-2/3`}>
+        <Text>sd</Text>
+      </View>
     </SafeAreaView>
     // <View>
     //   <TouchableOpacity
